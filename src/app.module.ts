65943b9dc71dea19c5176a9cf4010694cb14daf7
GGANGTONG <qkrds0914@gmail.com>
2024-03-18 20:00:20 +0900

    TypeOrmModuleOptions, env용 validationSchema 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { configModuleValidationSchema } from 'configs/envValidation.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmModuleOptions } from 'configs/database.config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+  isGlobal: true, validationSchema: configModuleValidationSchema
+  }),
+  TypeOrmModule.forRootAsync(typeOrmModuleOptions)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
